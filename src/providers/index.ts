908668ebc0187a4a2600f7a69ab33f57b27c9323
nikhilmac <nikhil.sharma.it@gmail.com>
2018-10-21 12:29:15 +0530

    new app replaces

--- src/providers/index.ts
+++
@@ -1,9 +0,0 @@
-export { Api } from './api/api';
-export { Items } from '../mocks/providers/items';
-export { Settings } from './settings/settings';
-export { User } from './user/user';
-
-export  { CourseUsers } from './course-users/course-users';
-export  { ChallengeUsers } from './challenge-users/challenge-users';
-export  { ComplaintUsers } from './complaint-users/complaint-users';
-export  { ComplaintFiles } from './complaintfiles/complaintfiles';
