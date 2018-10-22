9dd8c5e0390ea84f8922ecdcc72a00ce4b855f4f
nikhilmac <nikhil.sharma.it@gmail.com>
2018-10-22 10:12:46 +0530

    ShDb removed

--- src/providers/models/User.ts
+++ src/providers/models/User.ts
@@ -1,23 +1,16 @@
 export class User {
     id: number;
-    active: number;
-    first_update: number;
-    last_update: number;
-    user_name: string;
-    user_gender: string;
-    user_mobile: string;
-    user_address: number;
-    user_type: string;
-    user_id_proof: string;
-    user_id_proof_type: string;
-    user_url: string;
-    user_dob: number;
-    user_creator_id: number;
-    user_country_code: string;
-    user_ward_id: number;
-    user_push_reg_id: string;
-
-
     email: string;
     password: string;
+    token: string;
+    nombre: string;
+    primerApellido: string;
+    segundoApellido: string;
+    puntosAcumulados: string;
+    created_at: Date;
+    updated_at: Date;
+    blocked: boolean;
+    blocked_at: Date;
+    locale: string;
+
 }
